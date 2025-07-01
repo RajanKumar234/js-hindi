7c221f0cd7452377ae7d0ae1f6d76037ac85d2c9
Rajan Kumar Chauhan <154622806+RajanKumar234@users.noreply.github.com>
2025-07-01 08:53:07 +0000

    updated special loo[ concepts

--- 01_basics/special_Loops.js
+++ 01_basics/special_Loops.js
@@ -58,3 +58,116 @@
 // }
 
 // for in lloop does not works on map => is not itteratable
+
+
+// FOR EACH
+// const coding=["javascript","java","cpp","c"]
+// coding.forEach(function (val){
+//     console.log(val)
+// })
+
+// const coding=["javascript","java","cpp","c"]
+// // coding.forEach( (item) =>console.log(item))
+// coding.forEach( (item,index,arr) => {
+//      return console.log(item, index,arr)
+// } )
+
+// const coding=["javascript","java","cpp","c"]
+// function mycode(item){
+//     console.log(item)
+// }
+// coding.forEach(mycode)
+
+// const obj=[
+//     {
+//         companyName:"accenture",
+//         email:"acc121c@gmail.com"
+//     },
+//     {
+//         companyName:"amazon",
+//         email:"amazon121c@gmail.com"
+//     },
+//     {
+//         companyName:"blinkit",
+//         email:"blinkit121c@gmail.com"
+//     }
+// ]
+// obj.forEach((item)=>{
+//     return console.log(item.companyName)
+// })
+
+// const arr=[1,2,3,4,5]
+//     arr.forEach((item)=> {
+//     console.log(item)
+// })
+
+// const add = (item1)=> {
+//     return console.log(item1) //return console=> prints  underined
+// }
+// console.log(add(1))
+// output 1
+//        undefined
+
+// const nums=[1,2,3,4,5,6,7,8,9,10]
+
+// const value = nums.forEach((item)=> {
+//     console.log(item)// wil prints all the values 1 to 10
+//     return item
+// })
+// console.log(value)
+// note forEach does not return any value 
+
+//  filter :
+
+// const values = nums.filter((items)=> items >2)
+// const values = nums.filter((items)=>{ 
+//     return items >2 // implicit return
+// })
+// console.log(values)
+
+// const mynums=[]
+// nums.forEach((items) =>{
+//     if(items > 4){
+//         mynums.push(items)
+//     }
+// })
+// console.log(mynums)
+
+// data
+const books = [
+  { title: "Book 1", publish: "1990", genre: "history" },
+  { title: "Book 2", publish: "1991", genre: "fiction" },
+  { title: "Book 3", publish: "1992", genre: "science" },
+  { title: "Book 4", publish: "1993", genre: "fiction" },
+  { title: "Book 5", publish: "1994", genre: "history" },
+  { title: "Book 6", publish: "1995", genre: "technology" },
+  { title: "Book 7", publish: "1995", genre: "history" },
+  { title: "Book 8", publish: "1996", genre: "thriller" },
+  { title: "Book 9", publish: "1997", genre: "mystery" },
+  { title: "Book 10", publish: "1998", genre: "fiction" },
+  { title: "Book 11", publish: "1999", genre: "science" },
+  { title: "Book 12", publish: "2000", genre: "history" }
+];
+
+// let myBooks = books.filter((bk) => bk.genre == "history")
+//   myBooks = books.filter((bk) => bk.publish>= "1994")
+// console.log(myBooks)
+
+// const myBooks=books.filter((bk)=>{
+//     return bk.genre=="history" && bk.publish>1994
+// })
+// console.log(myBooks)
+// maps:
+
+const nums=[1,2,3,4,5,6,7,8,9]
+// const mynums=nums.map((item)=>item *10)
+// console.log(mynums)
+
+// chaining:
+
+// const mynums=nums.map((item)=>
+//   item *10)
+// .map((item)=>item+1)
+// .filter((item)=> item >33)
+// console.log(mynums)
+
